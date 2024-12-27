7d87a7f69a27d7aac6ff017ae429e3ef6342fddf
Alireza Aberoumand <alirezaaberoumand7192@gmail.com>
2024-12-26 23:11:28 -0800

    add course validator; define schema and validation rules for course attributes

---
+++ validators/courseValidator.js
@@ -0,0 +1,55 @@
+import Joi from "joi";
+
+const courseValidator = Joi.object({
+    title: Joi
+        .string()
+        .required()
+        .trim()
+        .min(3),
+    description: Joi
+        .string()
+        .required()
+        .trim()
+        .min(10),
+    cover: Joi
+        .string()
+        .required()
+        .trim(),
+    duration: Joi
+        .string()
+        .required()
+        .trim(),
+    support: Joi
+        .string()
+        .required()
+        .trim(),
+    price: Joi
+        .number()
+        .required()
+        .min(0),
+    href: Joi
+        .string()
+        .required()
+        .trim(),
+    status: Joi
+        .string()
+        .required()
+        .valid("completed", "recording", "pre-sale"),
+    discount: Joi
+        .number()
+        .required()
+        .min(0)
+        .max(100),
+    category: Joi
+        .string()
+        .required()
+        .trim(),
+    instructor: Joi
+        .string()
+        .required()
+        .trim(),
+}).strict();
+
+courseValidator.options({ abortEarly: false });
+
+export default courseValidator;
