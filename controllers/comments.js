1271a7882fdae0d3a845fcfbdf47d55e448eefc7
Alireza Aberoumand <maya7192@yahoo.com>
2024-12-31 22:45:21 -0800

    Filter comments to return only approved top-level comments and include replies in the response

--- controllers/comments.js
+++ controllers/comments.js
@@ -85,9 +85,10 @@ const getComments = async (req, res) => {
     try {
         //^ Get all comments from the database
         const comments = await Comment
-            .find({}, { createdAt: 0, updatedAt: 0, __v: 0 })
+            .find({isApproved: true, isItReply: false}, { createdAt: 0, updatedAt: 0, __v: 0 })
             .populate("course", { title: 1 })
-            .populate("creator", { password: 0, createdAt: 0, updatedAt: 0, __v: 0 });
+            .populate("creator", { password: 0, createdAt: 0, updatedAt: 0, __v: 0 })
+            .populate('replies', { createdAt: 0, updatedAt: 0, __v: 0 });
 
         //^ Return a 200 response with the comments
         return res.status(200).json(comments);
