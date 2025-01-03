6bba0e7a3d643ac47e807dabaf51a4bc5cfe0e7a
Alireza Aberoumand <maya7192@yahoo.com>
2025-01-02 22:50:34 -0800

    Add draft update functionality with validation and error handling

--- controllers/articles.js
+++ controllers/articles.js
@@ -99,7 +99,56 @@ const getDraftById = async (req, res) => {
     }
 };
 
-const updateDraft = async (req, res) => { };
+const updateDraft = async (req, res) => {
+    try {
+        //^ Get the draft ID from the request parameters
+        const { draftId } = req.params;
+
+        //^ Validate the draft ID
+        const { error: idValidationError } = idValidator.validate({ id: draftId });
+
+        //^ Return a 400 response if there is a validation error
+        if (idValidationError) {
+            throw new Error("Draft ID must be a valid ObjectId");
+        }
+
+        //^ Find the draft by ID
+        const draft = await Draft.findById(draftId);
+
+        //^ Return a 404 response if the draft does not exist
+        if (!draft) {
+            throw new Error("Draft not found");
+        }
+
+        //^ Get draft title and body from the request body
+        const { title: newTitle, body: newBody } = req.body;
+
+        //^ Create a draft test object
+        const draftSample = {
+            title: newTitle? newTitle : draft.title,
+            body: newBody? newBody : draft.body,
+        };
+        console.log(draftSample);
+
+        //^ Validate the draft object
+        const { error: draftValidationError } = draftValidator.validate(draftSample);
+
+        //^ Return a 400 response if there is a validation error
+        if (draftValidationError) {
+            throw new Error(draftValidationError);
+        }
+
+        //^ Update the draft object
+        await draft.updateOne(draftSample);
+
+        //^ Return a 200 response
+        return res.status(200).json("Draft updated successfully");
+    }
+    catch (error) {
+        //^ Return a 400 response if there is an error
+        return res.status(400).json({ message: error.message });
+    }
+};
 
 const deleteDraft = async (req, res) => { };
 
